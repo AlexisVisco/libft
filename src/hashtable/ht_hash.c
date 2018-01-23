8d218215cb2f52505c48118d996421f0f7bcb6bc
alexisvisco <alexis.viscogliosi@outlook.fr>
2018-01-23 18:46:42 +0100

    more releveant hash fn

--- src/hashtable/ht_hash.c
+++ src/hashtable/ht_hash.c
@@ -6,25 +6,30 @@
 /*   By: aviscogl <aviscogl@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/23 14:47:15 by aviscogl     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/23 18:08:41 by aviscogl    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/23 18:44:57 by aviscogl    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ht_hash(const char *str)
+size_t	ht_hash(const char *key)
 {
-	const size_t	len = ft_strlen(str);
-	uint32_t		hash;
-	uint32_t		i;
-
+	const size_t	len = ft_strlen(key);
+	size_t			hash;
+	size_t			i;
+	
 	i = 0;
-	hash = 0;
-	while(i < len) {
-		hash = hash << 8;
-		hash += str[i];
+	hash = i;
+	while (i < len)
+	{
+		hash += key[i];
+		hash += hash << 10;
+		hash ^= hash >> 6;
 		i++;
 	}
+    hash += (hash << 3);
+	hash ^= ( hash >> 11 );
+	hash += ( hash << 15 );
 	return (hash);
 }
